f4cd5cdedda104fca75f18efe3bc520cad18e383
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2017-09-29 09:23:07 -0400

    Setup easy testing

---
+++ web/modules/custom/big_red_bear/tests/src/Unit/LoadTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\Tests\big_red_bear\Functional;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Simple test to ensure that main page loads with module enabled.
+ *
+ * @group big_red_bear
+ */
+class LoadTest extends UnitTestCase {
+
+}
